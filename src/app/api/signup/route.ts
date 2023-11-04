044f55a4aebef8a5f4bfea1cdc7c0255aad6a767
saadabban76 <saadabban76@gmail.com>
2023-11-04 23:50:49 +0530

    solved mongodb error: you cant call req.json() 2 times 'can be wrong tho :)'

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -20,23 +20,23 @@ const signupInputs = z.object({
 export const POST = async (req: Request) => {
     await mongoose.connect(MdConnectionstring);
     // const { email, password } = signupSchema.parse(req.body)
-    const body = await req.json();
-    console.log('req : ', body);
-    const zodAuth = signupInputs.safeParse(body);
-    if (!zodAuth.success) {
-        return new Response(JSON.stringify({
-            // error: zodAuth.error.message as unknown,
-            message: 'invalid user credentials !'
-        }));
-    }
+    // const body = await req.json();
+    // console.log('req : ', body);
+    // const zodAuth = await signupInputs.safeParse(body);
+    // if (!zodAuth.success) {
+    //     return new Response(JSON.stringify({
+    //         message: 'invalid user credentials !'
+    //     }));
+    // }
 
     const { email, password } = await req.json();
     console.log('email : ', email);
     console.log('password : ', password);
     // check if the user already exist in the db
     const userExist = await User.findOne({ email, password });
+    console.log('user : ', userExist);
     if (userExist) {
-        console.log(JSON.stringify({ message: 'User Already Exist with this Email ID !' }));
+        // console.log(JSON.stringify({ message: 'User Already Exist with this Email ID !' }));
         return new Response(JSON.stringify({ message: 'User Already Exist with this Email ID !' }))
     } else {
         const newUser = new User({ email, password });
